4d1ada818d087fd2029120a75108e2d792595daa
AncientDaimyo <adfpbm@gmail.com>
2023-11-08 02:12:18 +0300

    Добавил контроллер регистрации

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,15 @@ use Symfony\Component\Routing\Annotation\Route;
 class AccountController extends AbstractController
 {
     #[Route('/account', name: 'app_account')]
-    public function index(): Response
+    public function index(AuthenticationUtils $authenticationUtils): Response
     {
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        $lastUsername = $authenticationUtils->getLastUsername();
+
         return $this->render('account/index.html.twig', [
-            'controller_name' => 'AccountController',
+            'last_username' => $lastUsername,
+            'error'         => $error,
         ]);
     }
 }
