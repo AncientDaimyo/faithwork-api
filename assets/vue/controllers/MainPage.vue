cf7bbb39e2f6721b801b643585c5451ccd83f20e
linnerrion <arcticshock15@gmail.com>
2023-11-08 16:40:00 +0300

    Parallax, Video, Marquee

--- assets/vue/controllers/MainPage.vue
+++ assets/vue/controllers/MainPage.vue
@@ -1,15 +1,8 @@
 <template>
     <main id="main_page">
-        <Promotion />
     </main>
 </template>
 
 <script>
-import Promotion from '../components/Promotion.vue';
 
-export default {
-    components: {
-        Promotion,
-    }
-}
 </script>
