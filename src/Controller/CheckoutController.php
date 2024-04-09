3845621adebe3d85b95b150d4c746c39ef9300f9
ADF <adfpbm@gmail.com>
2024-04-09 06:01:29 +0300

    replace checkoutData class to entity folder

--- src/Controller/CheckoutController.php
+++ src/Controller/CheckoutController.php
@@ -2,14 +2,12 @@
 
 namespace App\Controller;
 
-use phpDocumentor\Reflection\Types\This;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
-use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\Validator\Mapping\ClassMetadata;
+use App\Entity\CheckoutData;
 
 class CheckoutController extends AbstractController
 {
@@ -23,31 +21,26 @@ class CheckoutController extends AbstractController
 
 
             $errors = $this->co_data_validate($co_data, $validator);
-            if(!$errors)
-            {
+            if (!$errors) {
                 $response = new Response(
                     'Content',
                     Response::HTTP_OK,
                 );
                 $response->headers->set('Content-Type', 'application/json');
                 $response->setContent(json_encode(array('status' => 'OK')));
-            }
-            else
-            {
+            } else {
                 $response = new Response(
                     'Content',
                     Response::HTTP_I_AM_A_TEAPOT,
                 );
                 $response->headers->set('Content-Type', 'application/json');
-                $response->setContent(json_encode(array('errors' => $errors, 'status' => 'validation failed'))); 
+                $response->setContent(json_encode(array('errors' => $errors, 'status' => 'validation failed')));
             }
-
-        } else 
-        {
+        } else {
             $response = new Response(
                 'Content',
                 Response::HTTP_I_AM_A_TEAPOT,
-                ['content-type' => 'application/json']           
+                ['content-type' => 'application/json']
             );
             $response->headers->set('Content-Type', 'application/json');
             $response->setContent(json_encode(array('status' => 'data integrity has been violated')));
@@ -74,53 +67,15 @@ class CheckoutController extends AbstractController
             return false;
         }
     }
-    private function co_data_validate($co_data, ValidatorInterface $validator) {
+    private function co_data_validate($co_data, ValidatorInterface $validator)
+    {
         $errors = $validator->validate($co_data);
         if (count($errors) > 0) {
-    
+
             return $errors;
-        }
-        else {
+        } else {
             return false;
         }
     }
 }
-
-
-class CheckoutData
-{
-    public
-        $name,
-        $surname,
-        $patronomic,
-        $email,
-        $telephone,
-        $city,
-        $street,
-        $house,
-        $apartment;
-    function __construct(array $data)
-    {
-        $this->name         = $data['name'];
-        $this->patronomic   = $data['patronomic'];
-        $this->surname      = $data['surname'];
-        $this->email        = $data['email'];
-        $this->telephone    = $data['telephone'];
-        $this->city         = $data['city'];
-        $this->street       = $data['street'];
-        $this->house        = $data['house'];
-        $this->apartment    = $data['apartment'];
-    }
-    public static function loadValidatorMetadata(ClassMetadata $metadata)
-    {
-        $metadata->addPropertyConstraint('name',        new NotBlank());
-        $metadata->addPropertyConstraint('surname',     new NotBlank());
-        $metadata->addPropertyConstraint('patronomic',  new NotBlank());
-        $metadata->addPropertyConstraint('email',       new NotBlank());
-        $metadata->addPropertyConstraint('telephone',   new NotBlank());
-        $metadata->addPropertyConstraint('city',        new NotBlank());
-        $metadata->addPropertyConstraint('street',      new NotBlank());
-        $metadata->addPropertyConstraint('house',       new NotBlank());
-        $metadata->addPropertyConstraint('apartment',   new NotBlank());
-    }
-}
+   
