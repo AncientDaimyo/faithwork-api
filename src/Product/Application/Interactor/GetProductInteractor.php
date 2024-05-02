cb7f8bacb475e0b2304ea95387ade9e3b2df3f3e
ADF <adfpbm@gmail.com>
2024-05-02 22:43:05 +0300

    w

--- src/Product/Application/Interactor/GetProductInteractor.php
+++ src/Product/Application/Interactor/GetProductInteractor.php
@@ -30,24 +30,12 @@ class GetProductInteractor
     {
         $p = ProductRepositoryInterface::getProductFromRepositoryByUuid($repository, $uuid);
 
-        // $file = '%kernel.project_dir%/images/main/'. $p->getImage();
-        // $path = pathinfo($file);
-        // $ext = mb_strtolower($path['extension']);
-        // $img = "";
-        // if (in_array($ext, array('jpeg', 'jpg', 'gif', 'png', 'webp', 'svg'))) {
-        //     if ($ext == 'svg') {
-        //         $img = 'data:image/svg+xml;base64,' . base64_encode(file_get_contents($file));
-        //     } else {
-        //         $size = getimagesize($file);
-        //         $img = 'data:' . $size['mime'] . ';base64,' . base64_encode(file_get_contents($file));
-        //     }
-        // }
         $product_dto = array(
             'uuid'          => $p->getId(),
             'name'          => $p->getName(),
             'cost'          => $p->getCost(),
             'article'       => $p->getArticle(),
-            // 'image'         => $img,
+            'image'         => $p->getImage(),
             'description'   => $p->getDescriptionArr(),
             'sizes'         => $p->getSizesArr()
 
