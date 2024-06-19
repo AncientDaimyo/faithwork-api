cd5543eb8f0533708edce9ec076a1be4c74ee056
ADF <adfpbm@gmail.com>
2024-06-19 15:40:48 +0300

    rewrite dto on right pattern

---
+++ src/Checkout/Application/DTO/CartItemDTO.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Checkout\Application\DTO;
+
+class CartItemDTO
+{
+    public readonly string $uuid;
+    public readonly int $amount;
+    public readonly string $size;
+
+    public function __construct($uuid, $amount, $size)
+    {
+        $this->uuid     = $uuid;
+        $this->amount   = $amount;
+        $this->size     = $size;
+    }
+}
